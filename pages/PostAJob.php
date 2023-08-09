8297278ad539c747fd129437c99b29aca37ac55c
Kenneth Doerflein <ktd500@verizon.net>
2023-08-09 01:25:53 -0400

    PostAJob: add checkbox scripts disable, update total

--- pages/PostAJob.php
+++ pages/PostAJob.php
@@ -44,32 +44,70 @@
 
       <div class="mb-3">
         <div class="form-check">
-          <input required readonly name="basicPosting" type="checkbox" id="basicPosting" class="form-check-input" checked onclick="return false;">
+          <input value="150" required name="basicPosting" type="checkbox" id="basicPosting" class="form-check-input" checked onclick="return false;">
           <label title="" for="basicPosting" class="form-check-label">Basic Job Posting ($150)</label>
         </div>
       </div>
 
       <div class="mb-3">
         <div class="form-check">
-          <input name="support" type="checkbox" id="support" class="form-check-input" value="off">
+          <input value="79" name="support" type="checkbox" id="support" class="form-check-input" onclick="updateTotal(this)">
           <label title="" for="support" class="form-check-label">Receive 24-hour support for your job posting (+$79)</label>
         </div>
       </div>
 
+      <script>
+        function checkCheckboxStatus(chk) {
+          var chkName = document.getElementsByName(chk.name);
+          var chkID = document.getElementById(chk.id);
+          if (chkID.checked) {
+            for (var i = 0; i < chkName.length; i++) {
+              if (!chkName[i].checked) {
+                chkName[i].disabled = true;
+              } else {
+                chkName[i].disabled = false;
+              }
+            }
+          } else {
+            for (var i = 0; i < chkName.length; i++) {
+              chkName[i].disabled = false;
+            }
+          }
+          updateTotal(chk)
+        }
+
+        function updateTotal(chk) {
+          var chkID = document.getElementById(chk.id);
+          if (chkID.checked) {
+            var newTotal = parseFloat(total.getAttribute("value")) + parseFloat(chkID.getAttribute("value"));
+            document.getElementById("total").setAttribute("value", newTotal);
+          } else {
+            var newTotal = parseFloat(total.getAttribute("value")) - parseFloat(chkID.getAttribute("value"));
+            document.getElementById("total").setAttribute("value", newTotal);
+          }
+          document.getElementById("total").textContent = "Checkout Job Posting $" + newTotal;
+          document.getElementById("totalCost").setAttribute("value", newTotal);
+
+        }
+      </script>
+
       <div class="mb-3">
-        <div class="form-check"><input name="pinPost24hr" type="checkbox" id="pinPost24hr" class="form-check-input">
+        <div class="form-check">
+          <input value="99" name="pinAddons" type="checkbox" id="pinPost24hr" class="form-check-input" onclick="checkCheckboxStatus(this)">
           <label title="" for="pinPost24hr" class="form-check-label">Pin post on front page for 24 hours (+$99)</label>
         </div>
       </div>
 
       <div class="mb-3">
-        <div class="form-check"><input name="pinPost1wk" type="checkbox" id="pinPost1wk" class="form-check-input">
+        <div class="form-check">
+          <input value="199" name="pinAddons" type="checkbox" id="pinPost1wk" class="form-check-input" onclick="checkCheckboxStatus(this)">
           <label title="" for="pinPost1wk" class="form-check-label">Pin post on front page for 1 week (+$199)</label>
         </div>
       </div>
 
       <div class="mb-3">
-        <div class="form-check"><input name="pinPost1mth" type="checkbox" id="pinPost1mth" class="form-check-input">
+        <div class="form-check">
+          <input value="349" name="pinAddons" type="checkbox" id="pinPost1mth" class="form-check-input" onclick="checkCheckboxStatus(this)">
           <label title="" for="pinPost1mth" class="form-check-label">Pin post on front page for 1 month (+$349)</label>
         </div>
       </div>
@@ -92,10 +130,12 @@
 
       <div class="mb-3">
         <div class="">
-          <input required hidden="" readonly="" name="totalCost" type="checkbox" id="totalCost" class="form-check-input" value="150" checked="">
+          <input value="150" required hidden="" readonly="" name="totalCost" type="checkbox" id="totalCost" class="form-check-input" checked="">
         </div>
       </div>
-      <button type="submit" class="checkout-Button mt-4 mb-4 form-control btn btn-primary"><b>Checkout Job Posting $150</b></button>
+      <button type="submit" class="checkout-Button mt-4 mb-4 form-control btn btn-primary"><b>
+          <div value="150" id="total">Checkout Job Posting $150</div>
+        </b></button>
     </form>
   </div>
 
