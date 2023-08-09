448270a4c50b2d2546c5192deb0bdbecf7249c5d
Kenneth Doerflein <ktd500@verizon.net>
2023-08-08 23:04:03 -0400

    make navbar component for pages

--- pages/PostAJob.php
+++ pages/PostAJob.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <title>NewGradNomad</title>
+  <meta charset="utf-8">
+  <link rel="icon" href="../icon.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../style/NavBar.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script>
+    $(function(){
+      $("#navbar").load("navbar.html");
+    });
+  </script>
+</head>
+
+<body>
+<div id="navbar"></div>
+
+</body>
+
+</html>
