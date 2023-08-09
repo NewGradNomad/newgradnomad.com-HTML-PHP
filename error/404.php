b0cca2cfca04f19549951a424a8e4e7d04647ced
Kenneth Doerflein <ktd500@verizon.net>
2023-08-09 01:40:45 -0400

    add 403 and 404 error pages

--- error/404.php
+++ error/404.php
@@ -1,1 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <title>NewGradNomad</title>
+  <meta charset="utf-8">
+  <link rel="icon" href="../icon.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../style/NavBar.css" rel="stylesheet">
+  <link href="../style/About.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script>
+    $(function() {
+      $("#navbar").load("./errorNavbar.html");
+      $("#footer").load("../pages/footer.html");
+    });
+  </script>
+</head>
+
+<body>
+  <div id="navbar"></div>
+  <div class="text-center container">
+    <h1 class="mt-3">404 Not Found</h1>
+    <h3 class="mt-3">Oops! You seem to be lost.</h3>
+    <p>Here are some helpful links:</p>
+    <a role="button" href="../" class="mx-2 btn btn-primary" style="background-color: rgb(237, 111, 38); border: none;">Home</a>
+    <a role="button" href="../pages/PostAJob.php" class="mx-2 btn btn-primary" style="background-color: rgb(237, 111, 38); border: none;">Post a Job</a>
+    <a role="button" href="../pages/about.php" class="mx-2 btn btn-primary" style="background-color: rgb(237, 111, 38); border: none;">About</a>
+  </div>
+
+</body>
+<footer id="footer"></footer>
+
+</html>
