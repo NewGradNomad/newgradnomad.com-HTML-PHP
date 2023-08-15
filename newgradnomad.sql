d8f3f6ce8cbd0602ca2352bc2d272856bbd7b739
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2023-08-15 02:22:30 -0400

    update newgradnomad.sql

--- newgradnomad.sql
+++ newgradnomad.sql
@@ -19,7 +19,12 @@ GRANT SELECT, INSERT, DELETE, UPDATE ON newgradnomad.* TO ngn@localhost IDENTIFI
 -- Table structure for table `jobListings`
 --
 
-DROP TABLE IF EXISTS `jobListings`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobListings`
+--
+
 CREATE TABLE IF NOT EXISTS `jobListings` (
   `listingID` char(10) NOT NULL,
   `companyName` varchar(256) NOT NULL,
@@ -34,6 +39,7 @@ CREATE TABLE IF NOT EXISTS `jobListings` (
   `email` varchar(256) NOT NULL,
   `jobDescription` varchar(1028) NOT NULL,
   `postedDate` datetime NOT NULL,
+  `paymentStatus` int(10) NOT NULL,
   PRIMARY KEY (`listingID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,11 +47,13 @@ CREATE TABLE IF NOT EXISTS `jobListings` (
 -- Dumping data for table `jobListings`
 --
 
-INSERT INTO `jobListings` (`listingID`, `companyName`, `positionName`, `positionType`, `primaryTag`, `keywords`, `24hrSupport`, `highlightOrange`, `pin`, `url`, `email`, `jobDescription`, `postedDate`) VALUES
-('1107946366', 'Sporting Goods Emporium', 'Sheep Watcher', 'Full Time', 'Human Resource', 'Non Technical;Boring;', -1, 39, -1, 'https://www.google.com/search?q=Sporting%20Goods%20Emporium', '-1', 'You will watch our sheep to ensure they don\'t take over the world.', '2023-08-13 01:52:20'),
-('2449970057', 'NewGradNomad', 'Associate Software Engineer', 'Part Time', 'Software Development', 'Developer;Engineer;Full Stack;', -1, 39, 349, 'mailto:test@test.com', 'test@test.com', 'This is a job description for NewGradNomad.', '2023-08-13 01:35:18'),
-('4686589501', 'Bird Nerds', 'Bird Media Manager', 'Contract', 'Human Resource', 'Non Technical;', -1, -1, 199, 'https://birds.com', '-1', 'You will look and birds and describe them in extreme detail.', '2023-08-13 01:40:44'),
-('6725409691', 'Cloudx', 'Business Analyst', 'Part Time', 'Recruiter', 'Developer;Good Counter;', -1, -1, -1, 'mailto:counter@cloud.org', 'counter@cloud.org', 'You will count every cloud you see for 4 hours a day.', '2023-08-13 01:43:28');
+INSERT INTO `jobListings` (`listingID`, `companyName`, `positionName`, `positionType`, `primaryTag`, `keywords`, `24hrSupport`, `highlightOrange`, `pin`, `url`, `email`, `jobDescription`, `postedDate`, `paymentStatus`) VALUES
+('1107946366', 'Sporting Goods Emporium', 'Sheep Watcher', 'Full Time', 'Human Resource', 'Non Technical;Boring;', -1, 39, -1, 'https://www.google.com/search?q=Sporting%20Goods%20Emporium', '-1', 'You will watch our sheep to ensure they don\'t take over the world.', '2023-08-13 01:52:20', 1),
+('2449970057', 'NewGradNomad', 'Associate Software Engineer', 'Part Time', 'Software Development', 'Developer;Engineer;Full Stack;', -1, 39, 349, 'mailto:test@test.com', 'test@test.com', 'This is a job description for NewGradNomad.', '2023-08-13 01:35:18', 1),
+('3040697408', 'pin: 24 hour test', 'pin test: 2023-08-14 22:28:53', 'Part Time', 'Design', 'Full Stack;Accounting;', 79, 39, 99, 'https://pin.com', '-1', 'pin test', '2023-08-14 22:28:53', 1),
+('4686589501', 'Bird Nerds', 'Bird Media Manager', 'Contract', 'Human Resource', 'Non Technical;', -1, -1, 199, 'https://birds.com', '-1', 'You will look and birds and describe them in extreme detail.', '2023-08-13 01:40:44', 1),
+('6725409691', 'Cloudx', 'Business Analyst', 'Part Time', 'Recruiter', 'Developer;Good Counter;', -1, -1, -1, 'mailto:counter@cloud.org', 'counter@cloud.org', 'You will count every cloud you see for 4 hours a day.', '2023-08-13 01:43:28', 1),
+('9902379858', 'Stripe Testing', 'Stripe Testing', 'Full Time', 'Human Resource', 'Developer;', 79, 39, 99, 'mailto:Stripe@Testing.viu', 'Stripe@Testing.viu', 'Stripe Testing', '2023-08-15 01:13:20', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
