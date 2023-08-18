e13b5d3cd769bb313ae11f7b0a2c24179cb3b4b4
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2023-08-17 22:52:58 -0400

    refactor navbar (#12)
    * refactor navbar
    
    * change missed prices to new variables

--- error/403.php
+++ error/403.php
@@ -11,12 +11,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-  <script>
-    $(function() {
-      $("#navbar").load("./errorNavbar.html");
-      $("#footer").load("../pages/footer.html");
-    });
-  </script>
+  <script src="./error.js"></script>
 </head>
 
 <body>
