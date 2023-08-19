b39222cbb6b10b354ebbd25d6729b6992d65348f
Kenneth Doerflein <ktd500@verizon.net>
2023-08-19 02:59:17 -0400

    .htaccess: refactor error handling, add more error pages

---
+++ error/401.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>NewGradNomad</title>
+  <meta charset="utf-8">
+  <link rel="icon" href="../style/icon.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../style/NavBar.css" rel="stylesheet">
+  <link href="../style/About.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="../JavaScript/error.js"></script>
+</head>
+
+<body>
+  <div id="navbar"></div>
+  <div class="text-center container">
+    <h1 class="mt-3">401 Unauthorized</h1>
+    <h3 class="mt-3">Client Request Has Not Been Completed Because It Lacks Valid Authentication Credentials for the Requested Resource.</h3>
+    <div id="errorLinks"></div>
+  </div>
+
+</body>
+<footer id="footer"></footer>
+
+</html>
