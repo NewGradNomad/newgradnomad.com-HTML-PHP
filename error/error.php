845e5d29811517497ae250b249eddcd631504ab2
Kenneth Doerflein <ktd500@verizon.net>
2023-08-23 15:23:29 -0400

    refactor error pages

--- error/error.php
+++ error/error.php
@@ -12,17 +12,15 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-  <script src="../JavaScript/error.js"></script>
 </head>
 
 <body>
   <div id="navbar"></div>
-  <div class="text-center container">
-    <h1 class="mt-3">403 Forbidden</h1>
-    <h3 class="mt-3">You do not have access to this resource.</h3>
+  <div class="text-center container-fluid">
+    <div id="errorMessage"></div>
     <div id="errorLinks"></div>
   </div>
-
+  <script src="../JavaScript/error.js"></script>
 </body>
 <footer id="footer"></footer>
 
