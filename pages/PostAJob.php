dcf73af2db12730c51873a90e4f17bb95fc6aba7
Kenneth Doerflein <ktd500@verizon.net>
2023-09-03 15:01:41 -0400

    PostAJob: adjust spacing

--- pages/PostAJob.php
+++ pages/PostAJob.php
@@ -73,46 +73,52 @@ session_start();
 
     <div class="gray-form mt-4 container">
       <form name="jobForm" method="POST" action="../scripts/processPostAJob">
-        <label class="section-title mt-3 form-label"><b>Getting Started</b></label>
+        <label class="section-title my-3 form-label my-0"><b>Getting Started</b></label>
 
         <div class="mb-3">
-          <label class="form-label" for="companyName"><b>Company Name</b></label>
-          <small class="form-text" id="companyNameRequiredMessage" style="color: red !important;">* Required: Please fill out.</small>
+          <label class="form-label my-0" for="companyName"><b>Company Name</b></label>
+          <small class="form-text" id="companyNameRequiredMessage" style="color: red !important;">* Required</small>
+          <div class="mt-2"></div>
           <input autofocus required maxlength="200" placeholder="Enter Company Name" name="companyName" type="text" id="companyName" class="form-control" onkeyup="checkInputField(this)" />
           <div class="container"><small class="form-text">- Your company's brand name without business entities</small></div>
         </div>
 
         <div class="mb-3">
-          <label class="form-label" for="positionName"><b>Position</b></label>
-          <small class="form-text" id="positionNameRequiredMessage" style="color: red !important;">* Required: Please fill out.</small>
+          <label class="form-label my-0" for="positionName"><b>Position Name</b></label>
+          <small class="form-text" id="positionNameRequiredMessage" style="color: red !important;">* Required</small>
+          <div class="mt-2"></div>
           <input required maxlength="200" placeholder="Enter Position Name" name="positionName" type="text" id="positionName" class="form-control" onkeyup="checkInputField(this)" />
           <div class="container"><small class="form-text">- Write terms like "Associate Software Engineer" or "Social Media Manager" or "Business Analyst"</small></div>
         </div>
 
         <div class="mb-3">
-          <small class="form-text" id="positionTypeRequiredMessage" style="color: red !important;">* Required: Please fill out.</small>
+          <label class="form-label my-0" for="positionType"><b>Position Type</b></label>
+          <small class="form-text" id="positionTypeRequiredMessage" style="color: red !important;">* Required</small>
+          <div class="mt-2"></div>
           <select required class="form-select form-control" name="positionType" id="positionType" onchange="checkInputField(this)">
           </select>
           <div class="container"><small class="form-text">- Specify full-time, part-time, etc...</small></div>
         </div>
 
         <div class="mb-3">
-          <label class="form-label" for="primaryTag"><b>Primary Tag</b></label>
-          <small class="form-text" id="primaryTagRequiredMessage" style="color: red !important;">* Required: Please fill out.</small>
+          <label class="form-label my-0" for="primaryTag"><b>Primary Tag</b></label>
+          <small class="form-text" id="primaryTagRequiredMessage" style="color: red !important;">* Required</small>
+          <div class="mt-2"></div>
           <select required class="form-select form-control" name="primaryTag" id="primaryTag" onchange="checkInputField(this)">
           </select>
           <div class="container"><small class="form-text">- Main function of specified job</small></div>
         </div>
 
         <div class="mb-3">
-          <label class="form-label" for="keywords"><b>Keywords</b></label>
+          <label class="form-label my-0" for="keywords"><b>Keywords</b></label>
           <small class="form-text" id="keywordsRequiredMessage" style="color: red !important;">* Required: Max of 3.</small>
+          <div class="mt-2"></div>
           <select required class="form-select form-control" multiple="multiple" name="keywords[]" id="keywords" onchange="checkInputField(this)">
           </select>
           <div class="container"><small class="form-text">- Add keywords that pertain to the jobs purpose</small></div>
         </div>
 
-        <label class="mt-3 section-title form-label"><b>Job Post Perks</b></label>
+        <label class="mt-3 section-title form-label my-0"><b>Job Post Perks</b></label>
 
         <div class="mb-3">
           <div class="form-check">
@@ -149,26 +155,29 @@ session_start();
           </div>
         </div>
 
-        <label class="section-title form-label"><b>Job Details</b></label>
+        <label class="section-title form-label my-0"><b>Job Details</b></label>
         <div class="mb-3">
-          <label class="form-label" for="appURL"><b>Application URL</b></label>
+          <label class="form-label my-0" for="appURL"><b>Application URL</b></label>
           <small class="form-text" id="EmailURLRequiredMessage" style="color: red !important;">* Required: Please choose either email or URL.</small>
           <small class="form-text" id="URLFormatMessage" style="color: red !important;" hidden>* The URL must include https://</small>
+          <div class="mt-2"></div>
           <input required maxlength="200" placeholder="https://" name="appURL" type="url" id="appURL" class="form-control" onkeyup="checkEmailOrURL()" />
           <div class="container"><small class="form-text">- This is the job link applicants will be forwarded to in order to apply top your job</small></div>
         </div>
 
         <div class="mb-3">
-          <label class="form-label" for="appEmail"><b>Gateway Email Address</b></label>
+          <label class="form-label my-0" for="appEmail"><b>Gateway Email Address</b></label>
           <small class="form-text" id="EmailFormatMessage" style="color: red !important;" hidden>* This email is invalid, it needs to be in the format: name@example.com</small>
+          <div class="mt-2"></div>
           <input required maxlength="200" placeholder="name@example.com" name="appEmail" type="email" id="appEmail" class="form-control" onkeyup="checkEmailOrURL()" />
           <div class="container"><small class="form-text">- Applicant is routed to this email if no application url is provided!</small></div>
         </div>
 
         <div class="mb-3">
-          <label class="form-label" id="salaryRange"><b>Position Salary Range</b></label>
-          <small class="form-text" id="salaryRangeRequiredMessage" style="color: red !important;">* Required: Please fill out.</small>
+          <label class="form-label my-0" id="salaryRange"><b>Position Salary Range</b></label>
+          <small class="form-text" id="salaryRangeRequiredMessage" style="color: red !important;">* Required</small>
           <small hidden class="form-text" id="salaryRangeSwappedMessage" style="color: red !important;">* Max salary must be greater than min salary.</small>
+          <div class="mt-2"></div>
           <select required class="form-select form-control" name="salaryRangeMin" id="salaryRangeMin" onchange="checkSalaryRange()">
           </select>
           <div class="mt-2"></div>
@@ -177,9 +186,9 @@ session_start();
 
           <div class="container"><small class="form-text">- Please select a minimum and maximum salary from the select boxes</small></div>
         </div>
-        <label class="form-label"><b>Job Description</b></label>
-        <small class="form-text" id="jobDescRequiredMessage" style="color: red !important;">* Required: Please fill out.</small>
-        <div>
+        <label class="form-label my-0"><b>Job Description</b></label>
+        <small class="form-text" id="jobDescRequiredMessage" style="color: red !important;">* Required</small>
+        <div class="mt-2">
           <textarea required maxlength="1000" placeholder="" name="jobDesc" id="jobDesc" class="form-control" style="height: 150px;" onkeyup="checkInputField(this)"></textarea>
         </div>
 
