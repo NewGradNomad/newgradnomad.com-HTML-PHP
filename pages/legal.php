3e34341be2ceb737b8ac2c0a7a5862eadf4056da
Kenneth Doerflein <ktd500@verizon.net>
2023-09-14 14:45:18 -0400

    add footer page templates, finish links

---
+++ pages/legal.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>NewGradNomad</title>
+  <meta charset="utf-8">
+  <link rel="icon" href="../style/icon.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../style/NavBar.css" rel="stylesheet">
+  <link href="../style/Footer.css" rel="stylesheet">
+  <link href="../style/About.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="../JavaScript/about.js"></script>
+</head>
+
+<body>
+  <div id="navbar"></div>
+  <div class="container">
+    <h1 class="text-center mt-4">Legal and TOS</h1>
+  </div>
+
+</body>
+<footer id="footer"></footer>
+
+</html>
