d88a3b93a8e72fa2d106406040c43d8432ae78b8
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2024-01-28 12:18:44 -0500

    Update PostAJob.js

--- JavaScript/PostAJob.js
+++ JavaScript/PostAJob.js
@@ -1,10 +1,10 @@
-// window.addEventListener("pageshow", function (event) {
-//   var historyTraversal = event.persisted || (typeof window.performance != "undefined" && window.performance.navigation.type === 2);
-//   if (historyTraversal) {
+window.addEventListener("pageshow", function (event) {
+  var historyTraversal = event.persisted || (typeof window.performance != "undefined" && window.performance.navigation.type === 2);
+   if (historyTraversal) {
 //     // Handle page restore.
-//     window.location.reload();
-//   }
-// });
+     window.location.reload();
+   }
+ });
 
 window.onload = function () {
   checkEnableCheckoutButton()
